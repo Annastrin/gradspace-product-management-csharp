57be66da05cd9dbff9e65fa06da30e846ccbdf17
neste <neste@DELLLAPTOPANAST>
2023-06-19 12:43:53 +1200

    Add project files.

---
+++ ProductManagementBackend/Models/ProductManagementContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagementBackend.Models;
+
+public partial class ProductManagementContext : DbContext
+{
+    public ProductManagementContext()
+    {
+    }
+
+    public ProductManagementContext(DbContextOptions<ProductManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Image).HasColumnType("text");
+            entity.Property(e => e.Price).HasColumnType("money");
+            entity.Property(e => e.Title)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
